24d40cff0574171744bfa81929c9d45973be0e7b
Joshua Moore <Jmoore0177@conestogac.on.ca>
2023-01-29 15:47:21 -0500

    first commit, figuring out how to add unity projects to github not just VS solutions

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rig;
+    public float moveSpeed;
+    public float jumpForce;
+
+    private bool isGrounded;
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        float z = Input.GetAxisRaw("Vertical") * moveSpeed;
+
+        rig.velocity = new Vector3(x,rig.velocity.y,z);
+
+
+        Vector3 vel = rig.velocity;
+        vel.y= 0;
+        if(vel.x != 0 || vel.z != 0 )
+        {
+            //set player forward direction to velocity direction
+            transform.forward = vel;
+        }
+        
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.GetContact(0).normal == Vector3.up)
+        {
+            isGrounded = true;
+        }
+    }
+}
